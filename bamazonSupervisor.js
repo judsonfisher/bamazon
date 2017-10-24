49d846bc88bd0d22cd95d885fa605bcbc0d89a6d
Your Namejudsonfisher <judsonefisher@gmail.com>
2017-10-24 17:50:12 -0500

    finishing up

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -3,24 +3,89 @@ var inquirer = require("inquirer");
 var Table = require('easy-table');
 var t = new Table;
 
-// Create inquirer function that runs two options:
-//    * View Product Sales by Department
-//    * Create New Department
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  start();
+});
+
+function start() {
+	inquirer.prompt([
+	{
+		name: 'choice',
+		type: 'list',
+		choices: ['View Product Sales by Department', 'Create New Department'],
+		message: 'Which option would you like to select from?'
+	}
+	 ]).then(function(answers) {
+	  	if (answers.choice === 'View Product Sales by Department') {
+	  		return depSales();
+	  	} else if (answers.choice === 'Create New Department') {
+	  		return createDepartment();
+	  	};
+	  });
+}
+
+function depSales() {
+	console.log("This operation is still in the works. Please ckeck back later! :)");
+	connection.end();
+};
+
+
+function createDepartment() {
+	inquirer.prompt([
+		{
+			name: 'name',
+			type: 'input',
+			message: "What is name of the new department?",
+		},
+		{
+	        name: "costs",
+	        type: 'input',
+	        message: "What are the overhead costs associated with this department?",
+	        validate: function(value) {
+	          if (isNaN(value) === false) {
+	            return true;
+	          }
+	          return false;
+	        }
+	    },
+		]).then(function(answers) {
+			var name = answers.name
+			var costs = answers.costs;
+			var values = { 
+				department_name: name,
+				over_head_costs: costs
+			};
+			connection.query('INSERT INTO departments SET ?', values, function (error, results, fields) {
+			  if (error) throw error;
+			  console.log('This department has been created. Go hire some people.');
+			  connection.end();
+			});
+		});
+};
 
 
 
 
-
-// When user selects `View Product Sales by Department`, a summarized table in their terminal/bash window. Use the table below as a guide.
-// | department_id | department_name | over_head_costs | product_sales | total_profit |
-// | ------------- | --------------- | --------------- | ------------- | ------------ |
-// | 01            | Electronics     | 10000           | 20000         | 10000        |
-// | 02            | Clothing        | 60000           | 100000        | 40000        |
-
-// The `total_profit` column should be calculated on the fly using the difference between `over_head_costs` and `product_sales`. `total_profit` should not be stored in any database. You should use a custom alias.
-
-// If you can't get the table to display properly after a few hours, then feel free to go back and just add `total_profit` to the `departments` table.
-//    * Hint: You may need to look into aliases in MySQL.
-//    * Hint: You may need to look into GROUP BYs.
-//    * Hint: You may need to look into JOINS.
-//    * **HINT**: There may be an NPM package that can log the table to the console. What's is it? Good question :)
+// var data = [];
+// 	connection.query("SELECT * FROM departments", function(err, results) {
+// 		if (err) throw err;
+// 		var id;
+// 		var name;
+// 		var overhead;
+// 		for (var x = 0; x < results.length; x++) {
+// 			console.log(results[x].item_id, results[x].product_name, results[x].department_name, results[x].price, results[x].stock_quantity);
+// 		};
+// 	});
